4fc9186a64409f2f88a37ee782f78e05e402fb2c
safi-io <m.safi.ullah@outlook.com>
2024-08-28 10:12:34 +0500

    Implemented User Route

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
-
-dotenv.config();
-
 import connectDB from "./config/database.js";
-connectDB();
-
-const app = express();
+import userRoute from "./routes/user-route.js";
 
 const PORT = process.env.PORT || 7000;
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(urlencoded({ extended: false }));
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => console.log("Server on", PORT));
