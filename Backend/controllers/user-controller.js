55419b795408dd4010d35f1ada36e1f561f45ba6
safi-io <m.safi.ullah@outlook.com>
2024-08-28 10:01:54 +0500

    Developed User Controller for Registration.

---
+++ Backend/controllers/user-controller.js
@@ -0,0 +1,42 @@
+import user from "../models/user-model";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    // Checking that no field is empty.
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    // Checking that pass & confirmPass are same.
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "Password and Confirm Password must be same." });
+    }
+
+    // Checking if username already exists.
+    const userData = await user.findOne({ username });
+    if (userData) {
+      return res.status(400).json({ message: "Username already exist." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Profile Photo
+    const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    await user.create({
+      username,
+      fullName,
+      password: hashedPassword,
+      gender,
+      profilePhoto: gender === "male" ? maleProfilePhoto : femaleProfilePhoto,
+    });
+  } catch (error) {
+    console.log(`Error in User Controller : ${error}`);
+  }
+};
