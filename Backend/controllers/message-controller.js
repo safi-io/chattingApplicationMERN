abd5288de44302fb82f93bd425d845321178d6f3
safi-io <m.safi.ullah@outlook.com>
2024-09-03 11:21:22 +0500

    Minor Fixes

--- Backend/controllers/message-controller.js
+++ Backend/controllers/message-controller.js
@@ -1,6 +1,8 @@
 import converstaion from "../models/conversation-model.js";
 import message from "../models/message-model.js";
 
+// Sending Message Controller
+
 export const sendMessage = async (req, res) => {
   try {
     const senderId = req.id;
@@ -37,9 +39,11 @@ export const sendMessage = async (req, res) => {
 
     // SOCKET IO
   } catch (error) {
-    console.log("Send Message Controller", error);
+    console.log("Error in Send-Message Controller", error);
   }
 };
+
+// Getting Message Controller
 
 export const getMessage = async (req, res) => {
   try {
@@ -51,8 +55,9 @@ export const getMessage = async (req, res) => {
         participants: { $all: [senderId, receiverId] },
       })
       .populate("messages");
+    // If conversation is found, return all messages
     return res.status(200).json(getConversation?.messages);
   } catch (error) {
-    console.log(`Error in Message Controller ${error}`);
+    console.log(`Error in Get-Message Controller ${error}`);
   }
 };
