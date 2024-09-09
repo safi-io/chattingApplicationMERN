67e411b26a74d5f9dbe82ea3ac6e6027b3c099db
safi-io <m.safi.ullah@outlook.com>
2024-09-09 05:30:24 +0500

    Seeting up Tailwind CSS for Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <> 
-    <h1>I am from App component.</h1>
+    <h1 className='bg-red-500 text-white'>I am from App component.</h1>
     </>
   );
 }
