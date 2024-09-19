08c30d3d768221429188cf3cb4e815db5fc9ee22
safi-io <m.safi.ullah@outlook.com>
2024-09-19 15:51:44 +0500

    Corrected  Paths for DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/database.js";
+import connectDB from "./database.js";
 import userRoute from "./routes/user-route.js";
 import messageRoute from "./routes/message-route.js";
 import cookieParser from "cookie-parser";
